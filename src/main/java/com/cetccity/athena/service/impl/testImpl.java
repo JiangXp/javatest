743e2f4f04cd9ddfe925abe451c2bcbc4d1e6009
Jiangxp <1813250264@qq.com>
2020-04-20 15:39:37 +0800

    add test impl file

---
+++ src/main/java/com/cetccity/athena/service/impl/testImpl.java
@@ -0,0 +1,9 @@
+package com.cetccity.athena.service.impl;
+
+import com.cetccity.athena.service.test;
+
+public class testImpl implements test {
+    public String test(String name) {
+        return null;
+    }
+}
