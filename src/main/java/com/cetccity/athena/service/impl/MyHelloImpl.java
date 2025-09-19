af537c5de2aa93df377e7b377327ee30941573a3
Jiangxp <1813250264@qq.com>
2025-09-19 16:38:07 +0800

    11111

--- src/main/java/com/cetccity/athena/service/impl/MyHelloImpl.java
+++ src/main/java/com/cetccity/athena/service/impl/MyHelloImpl.java
@@ -5,6 +5,7 @@ import com.cetccity.athena.service.MyHello;
 public class MyHelloImpl implements MyHello {
     public void hello() {
         System.out.println("hello");
+        System.out.println("dfasdfsd");
 
     }
 }
